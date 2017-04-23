7f3d93219266ce234af99c2859dbbd07530412b6
hong <1043518275@qq.com>
2017-04-23 22:21:50 +0800

    国际化相关处理和Thymeleaf 模板语法的基本使用

--- Chapter4-Thymeleaf/src/main/java/com/hong/Application.java
+++ Chapter4-Thymeleaf/src/main/java/com/hong/Application.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
