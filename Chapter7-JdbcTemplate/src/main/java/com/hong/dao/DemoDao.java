b744cdb4618fb75558b898388767af96c1b77892
hong <1043518275@qq.com>
2017-04-25 15:22:41 +0800

    spring boot 整合jdbcTemplate 操作数据库.

---
+++ Chapter7-JdbcTemplate/src/main/java/com/hong/dao/DemoDao.java
@@ -0,0 +1,32 @@
+package com.hong.dao;
+
+import com.hong.domain.Demo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by hong on 2017/4/25.
+ */
+@Repository
+public class DemoDao{
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    /**
+     * 通过id获取demo对象.
+     * @param id
+     * @return
+     */
+    public Demo  getById(Long id){
+        String sql = "select * from t_demo where id=?";
+        RowMapper<Demo> rowMapper = new BeanPropertyRowMapper<Demo>(Demo.class);
+        return jdbcTemplate.queryForObject(sql, rowMapper,id);
+    }
+
+
+}
