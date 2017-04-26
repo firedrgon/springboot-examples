75264a9a5c8332375edd69a2d7f5ba1bb91dbfe7
hong <1043518275@qq.com>
2017-04-26 17:52:30 +0800

    spring boot 整合JdbcTemplate 多数据源

---
+++ Chapter8-JdbcTemplate-Mutil-Datasource/src/test/java/com/hong/MutilDatasourceTest.java
@@ -0,0 +1,45 @@
+package com.hong;
+
+import com.hong.database.DataSourceConfig;
+import com.hong.domain.Demo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * Created by hong on 2017/4/26.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootApplication(scanBasePackageClasses={Application.class, DataSourceConfig.class})
+public class MutilDatasourceTest {
+
+    @Autowired
+    @Qualifier("primaryJdbcTemplate")
+    private JdbcTemplate primaryJdbcTemplate;
+
+    @Autowired
+    @Qualifier("secondaryJdbcTemplate")
+    private JdbcTemplate secondaryJdbcTemplate;
+
+
+    @Test
+    public void test(){
+        String sql = "select * from demo ";
+        RowMapper<Demo> rowMapper = new BeanPropertyRowMapper<>(Demo.class);
+        List<Demo> demos= primaryJdbcTemplate.query(sql,rowMapper);
+        demos.forEach(e-> System.out.println(e.toString()));
+
+        List<Demo> demos2 =secondaryJdbcTemplate.query(sql,rowMapper);
+        demos2.forEach(demo2 -> System.out.println(demo2.toString()) );
+
+    }
+
+}
