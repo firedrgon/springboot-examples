8f6b4b8ab955c303c120064b8d2d36a53d0f1b90
hong <1043518275@qq.com>
2017-05-08 14:13:43 +0800

    Spring Boot框架之Redis实现Session共享

---
+++ Chapter9-Redis-Session/src/main/java/com/hong/RedisSessionConfig.java
@@ -0,0 +1,27 @@
+package com.hong;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * Created by hong on 2017/5/8.
+ */
+//@Configuration
+/**
+ *  通过加上@EnableRedisHttpSession注解，开启redis集中式session管理，所有的session都存放到了redis中
+ *  maxInactiveIntervalInSeconds 配置session 失效时间
+ * **/
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400 * 30)
+public class RedisSessionConfig {
+
+    /**
+     * 配置实用Jedis 连接池的方式 连接Redis服务（默认就是使用的這种方式，所以可以不配）
+     * @return
+     */
+//    @Bean
+//    public JedisConnectionFactory connectionFactory(){
+//        return  new JedisConnectionFactory();
+//    }
+
+
+}
