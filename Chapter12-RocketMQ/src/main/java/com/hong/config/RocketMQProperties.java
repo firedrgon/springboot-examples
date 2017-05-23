0619d61c1d67a3052c4fc4f323c239f2432d523a
hong <1043518275@qq.com>
2017-05-23 15:43:17 +0800

    Spring boot 整合RocketMQ 测试.

---
+++ Chapter12-RocketMQ/src/main/java/com/hong/config/RocketMQProperties.java
@@ -0,0 +1,16 @@
+package com.hong.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by hong on 2017/5/23.
+ */
+@Configuration
+public class RocketMQProperties {
+
+    private String namesrvAddr;
+    private String instanceName;
+    private String clientIP;
+//    private ProducerConfig producer;
+//    private ConsumerConfig consumer;
+}
