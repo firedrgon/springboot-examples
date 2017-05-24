71a453bbffacaefba701955197e344091b0972cd
hong <1043518275@qq.com>
2017-05-24 17:47:45 +0800

    配置文件详解：自定义属性、随机数、多环境配置等

--- Chapter2/src/main/java/com/hong/Application.java
+++ Chapter2/src/main/java/com/hong/Application.java
@@ -10,6 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        // 注意：如果改变了spring boot 配置文件的位置，在启动的时候指定位置
+        //       测试时，@RunWith(SpringRunner.class)
+        //               @SpringBootTest(classes= PlutoIsOmsWebApplication.class)
+        //               @TestPropertySource(locations="classpath:conf/env/application.properties")
+        //        也需要指定下；
+        // System.setProperty("spring.config.location","classpath:conf/env/application.properties");
         SpringApplication.run(Application.class,args);
     }
 }
