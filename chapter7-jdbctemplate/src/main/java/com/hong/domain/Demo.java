d5524c044a996391c9cdb24283fab05ba0ef8546
hong <1043518275@qq.com>
2017-06-07 12:52:47 +0800

    spring boot 整合JdbcTemplate.

---
+++ chapter7-jdbctemplate/src/main/java/com/hong/domain/Demo.java
@@ -0,0 +1,40 @@
+package com.hong.domain;
+
+
+
+/**
+ * @ClassName: Demo
+ * @Description:
+ * @author hong
+ * @date 2017/4/25
+ * @version v1.1
+ */
+public class Demo {
+
+    private long id;//主键.
+    private String name;//测试名称.
+
+
+    public Demo() {
+    }
+
+    public Demo(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
