9072c6cbce4271b7deb552cf3bb9244e3a947f95
hong <1043518275@qq.com>
2017-06-07 12:54:38 +0800

    Spring Boot 整合mybatis.

---
+++ chapter7-mybatis/src/main/java/com/hong/Application.java
@@ -0,0 +1,16 @@
+package com.hong;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hong on 2017/4/25.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
