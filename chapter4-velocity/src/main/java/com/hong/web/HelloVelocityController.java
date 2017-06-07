df1d79125959ac0717ccfc6edc257aa78692beb5
hong <1043518275@qq.com>
2017-06-07 12:46:57 +0800

    Spring Boot 使用velocity渲染web视图

---
+++ chapter4-velocity/src/main/java/com/hong/web/HelloVelocityController.java
@@ -0,0 +1,20 @@
+package com.hong.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by hong on 2017/4/23.
+ */
+@Controller
+public class HelloVelocityController {
+
+
+
+    @RequestMapping("/hello")
+    public String index(Model model){
+        model.addAttribute("name","hong");
+        return "index";
+    }
+}
