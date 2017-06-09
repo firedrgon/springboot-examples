eb418ff08bac331e353a423c0fbfd325d52f6f0e
hong <1043518275@qq.com>
2017-06-09 17:44:06 +0800

    Spring Boot 使用velocity渲染web视图

---
+++ chapter4-velocity/src/main/java/com/hong/config/Velocity.java
@@ -0,0 +1,22 @@
+package com.hong.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.velocity.VelocityLayoutViewResolver;
+
+/**
+ * Created by hong on 2017/6/9.
+ */
+//@Configuration
+//public class Velocity {
+//
+//
+//    @Bean(name = "velocityViewResolver")
+//    public VelocityLayoutViewResolver velocityViewResolver(VelocityProperties properties) {
+//        VelocityLayoutViewResolver resolver = new VelocityLayoutViewResolver();
+//        properties.applyToViewResolver(resolver);
+//        resolver.setLayoutUrl("layout/default.vm");
+//        return resolver;
+//    }
+//
+//}
